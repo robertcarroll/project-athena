87430884dd0470205eead6473fbc8b3ef0340db7
Bobby <bobby@robertcarroll.io>
2023-04-01 12:54:26 -0400

    add auth routing file

---
+++ src/app/modules/auth/auth.routing.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '@modules/auth/login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'signedout',
+        component: LoginComponent
+      },
+      {
+        path: 'callback',
+        redirectTo: '/dashboard'
+      }
+    ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
