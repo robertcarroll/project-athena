9b411bf6bf2a47d9991ae0365ac3b4b943cf53b4
Bobby <bobby@robertcarroll.io>
2023-04-01 21:54:34 -0400

    update base routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterModule, RouterStateSnapshot, Routes } from '@angular/router';
+import { AuthGuard } from '@core/guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    canActivate: [(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(AuthGuard).canActivate(route, state)],
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('src/app/modules/home/home.module').then(m => m.HomeModule)
+      }
+    ]
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('src/app/modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  // Fallback when no prior routes is matched
+  { path: '**', redirectTo: '/auth/login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
